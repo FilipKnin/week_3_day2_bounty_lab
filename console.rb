92647be5599c12f2f31a16874d8fb6a2f5704094
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-14 15:15:36 +0100

    bounty1, bounty2 created

---
+++ console.rb
@@ -0,0 +1,16 @@
+require ("pry-byebug")
+require_relative("models/bounty")
+
+bounty1 = Bounty.new({
+  "name" => "Raphael",
+  "species" => "human",
+  "bounty_value" => "250",
+  "danger_level" => "low"
+  })
+
+bounty2 = Bounty.new({
+  "name" => "Tony",
+  "species" => "latin",
+  "bounty_value" => "15000",
+  "danger_level" => "high"
+  })
